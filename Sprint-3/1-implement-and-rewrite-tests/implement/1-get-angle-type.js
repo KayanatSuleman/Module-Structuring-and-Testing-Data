ea62057320bdf00043b3dd14c9dbc4e6aa3c157b
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-17 21:28:55 +0000

    Adding to the getAngleType function the straight angle if statement

--- Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/1-get-angle-type.js
@@ -30,6 +30,10 @@ function getAngleType(angle) {
   if (angle > 90 && angle < 180) {
     return "Obtuse angle";
   }
+
+  if (angle === 180) {
+    return "Straight angle";
+  }
 }
 
 // The line below allows us to load the getAngleType function into tests in other files.
