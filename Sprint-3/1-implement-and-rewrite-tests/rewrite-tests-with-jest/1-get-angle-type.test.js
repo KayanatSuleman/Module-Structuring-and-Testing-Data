57d3ea59a423c5b08d7062ad732ca5ec3df59163
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-19 10:28:48 +0000

    Adding the jest test case for Straight Angle

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -24,6 +24,11 @@ test('should return "Obtuse angle" when (90 < angle < 180)', () => {
   expect(getAngleType(120)).toEqual("Obtuse angle");
   expect(getAngleType(179)).toEqual("Obtuse angle");
 })
+
 // Case 4: Straight angle
+test('should return "Straight angle" when angle is 180', () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+})
+
 // Case 5: Reflex angles
 // Case 6: Invalid angles
