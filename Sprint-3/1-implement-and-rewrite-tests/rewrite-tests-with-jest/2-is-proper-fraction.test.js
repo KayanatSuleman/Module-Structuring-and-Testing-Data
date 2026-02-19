eacafaddb59ee9e0819f7c619aeaf664ede2c4b9
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-19 19:45:29 +0000

    Updating test case names to be more descriptive and accurate

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -4,13 +4,13 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 
 // TODO: Write tests in Jest syntax to cover all combinations of positives, negatives, zeros, and other categories.
 
-// Special case: numerator is zero
+// Special case: denominator is zero
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
 
 // Numerator is zero
-test(`should return false when numerator is zero`, () => {
+test(`should return true when numerator is zero and denominator is non-zero`, () => {
   expect(isProperFraction(0, 1)).toEqual(true);
 }); 
 
