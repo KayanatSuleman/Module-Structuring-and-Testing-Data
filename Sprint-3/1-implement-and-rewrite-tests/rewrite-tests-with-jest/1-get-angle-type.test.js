fae1841db806135b0d7932b32107dfe245a61354
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-19 10:23:51 +0000

    Adding the jest test case for Obtuse Angle

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/1-get-angle-type.test.js
@@ -19,6 +19,11 @@ test('should return "Right angle" when angle is 90', () => {
 });
 
 // Case 3: Obtuse angles
+test('should return "Obtuse angle" when (90 < angle < 180)', () => {
+  expect(getAngleType(91)).toEqual("Obtuse angle");
+  expect(getAngleType(120)).toEqual("Obtuse angle");
+  expect(getAngleType(179)).toEqual("Obtuse angle");
+})
 // Case 4: Straight angle
 // Case 5: Reflex angles
 // Case 6: Invalid angles
