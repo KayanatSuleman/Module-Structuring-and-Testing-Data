ebb1483f677f7a3a73f9c82b98e08ae30c57e04e
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-02-19 19:33:50 +0000

    Adding the jest test case for zero numerator and denominator and proper fractions

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -8,3 +8,19 @@ const isProperFraction = require("../implement/2-is-proper-fraction");
 test(`should return false when denominator is zero`, () => {
   expect(isProperFraction(1, 0)).toEqual(false);
 });
+
+// Numerator is zero
+test(`should return false when numerator is zero`, () => {
+  expect(isProperFraction(0, 1)).toEqual(true);
+}); 
+
+//Proper fractions 
+test("should return true for proper fractions", () => {
+  expect(isProperFraction(1,2)).toEqual(true);
+  expect(isProperFraction(2,5)).toEqual(true);
+})
+
+//Equal Numerator and Denominator  
+test("should return false when numerator equals denominator", () => {
+  expect(isProperFraction(2,2)).toEqual(false);
+})
